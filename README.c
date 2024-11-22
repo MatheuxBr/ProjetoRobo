c5a2c7d0e05022f5a6284fc6e0de18aecb2bcfe1
MatheuxBr <matheus.garcia4@hotmail.com>
2024-11-22 19:19:48 -0300

    Add files via upload

---
+++ README.c
@@ -0,0 +1,24 @@
+<h1 align = "center">Gigantes De MDF</h1>
+
+Projeto referente à disciplina de Programação de Hardware, lecionada pelo professor Sergio Schina, do curso de Engenharia de Computação da Universidade Santa Cecília.
+
+## Participantes
+
+- Matheus Garcia
+- Thiago Taveira
+- Vinicius Morão
+- Matheus Yamauti
+
+## Sobre
+
+O objetivo principal deste projeto consiste em aplicar os conhecimentos adquiridos ao longo do curso. O trabalho abrange o planejamento e a concepção dos componentes envolvidos, o desenvolvimento da estrutura e a programação do código-fonte de um robô destinado ao microcontrolador Atmega-328p, utilizando a linguagem C. O robô projetado deverá ser capaz de realizar movimentos, direcionar um feixe de laser, efetuar disparos e executar outros comportamentos conforme estabelecido no edital.
+
+## Softwares
+
+- Arduino IDE
+- AutoCAD
+- Doxygen
+- KiCad
+- PCB-Proto
+- RDWorks
+- SimulIDE
