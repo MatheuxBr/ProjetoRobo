67708f4426727d1bb536e4fcf07a7c3d4ce1e363
MatheuxBr <matheus.garcia4@hotmail.com>
2024-11-23 09:28:03 -0300

    Update README.c

--- README.c
+++ README.c
@@ -1,3 +1,6 @@
+# Projeto Robo
+ 
+<h1 align = "center">Projeto Robô</h1>
 
 Projeto referente à disciplina de Programação de Hardware, lecionada pelo professor Sergio Schina, do curso de Engenharia de Computação da Universidade Santa Cecília.
 
@@ -11,6 +14,15 @@ Projeto referente à disciplina de Programação de Hardware, lecionada pelo pro
 ## Sobre
 
 O objetivo principal deste projeto consiste em aplicar os conhecimentos adquiridos ao longo do curso. O trabalho abrange o planejamento e a concepção dos componentes envolvidos, o desenvolvimento da estrutura e a programação do código-fonte de um robô destinado ao microcontrolador Atmega-328p, utilizando a linguagem C. O robô projetado deverá ser capaz de realizar movimentos, direcionar um feixe de laser, efetuar disparos e executar outros comportamentos conforme estabelecido no edital.
+
+## Componentes
+
+- Atmega328p
+- Sensor Infravermelho (Vs1838)
+- LDR 20mm
+- Laser Vermelho
+- Motor DC 3v-6v
+- LEDs
 
 ## Softwares
 
